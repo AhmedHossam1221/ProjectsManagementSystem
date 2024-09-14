d2dbade0cc8f31ac32582db3958379c3745648d6
RaghadSameeh <RaghadSameehElsharkawy@gmail.com>
2024-09-15 00:10:06 +0300

    ResolveVerifyAccountIssuee

--- ProjectsManagement/Repositories/Base/IRepository.cs
+++ ProjectsManagement/Repositories/Base/IRepository.cs
@@ -17,7 +17,5 @@ namespace ProjectsManagement.Repositories.Base
         Task SaveChangesAsync();
         IQueryable<T> GetAllAsync(Expression<Func<T, bool>> predicate);
         Task<T> FirstAsync(Expression<Func<T, bool>> predicate);
-        Task<T> FirstAsyncWithTracking(Expression<Func<T, bool>> predicate);
-
     }
 }
--- ProjectsManagement/Repositories/Base/Repository.cs
+++ ProjectsManagement/Repositories/Base/Repository.cs
@@ -77,10 +77,5 @@ namespace ProjectsManagement.Repositories.Base
         {
             return await GetAllAsync(predicate).FirstOrDefaultAsync();
         }
-
-        public async Task<T> FirstAsyncWithTracking(Expression<Func<T, bool>> predicate)
-        {
-            return await GetAllAsync(predicate).AsTracking().FirstOrDefaultAsync();
-        }
     }
 }
